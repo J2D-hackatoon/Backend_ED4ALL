4366d939f264a8c35d551c0c1494526ed0ea1cdb
marinaariasq <88584116+marinaariasq@users.noreply.github.com>
2023-11-16 17:54:07 +0100

    Update the comments from the code

--- 04_generacion_dataset_final.py
+++ 04_generacion_dataset_final.py
@@ -8,7 +8,6 @@ Created on Thu Nov 16 13:58:50 2023
 import pandas as pd
 
 # Realizamos la lectura de los datos
-
 file_in = "Input_Data/centros_reglados_filtrados.csv"
 centros_reglados = pd.read_csv(file_in)
 
@@ -23,10 +22,9 @@ centros_info = pd.concat([centros_no_reglados,centros_reglados])
 centros_info.to_csv('Input_Data\centros_unificados.csv', index=False)  
 
 # procedemos a unificar el dataframe de rentas con el de centros
-
 df_unificado = pd.merge(renta_promedio, centros_info, left_on='neighborhood_code', right_on = 'addresses_neighborhood_id')
 
-# procedemos a unificar la adressa
+# generamos una variable "adress" que englobe addresses_road_name y addresses_start_street_number
 df_unificado['address'] = df_unificado['addresses_road_name'] + ' ' + df_unificado['addresses_start_street_number'].astype(int).astype(str)
 df_unificado['avg_income'] = df_unificado.groupby('district_code')['gross_income'].transform('mean')
 
@@ -35,11 +33,12 @@ df_unificado = df_unificado.drop(['addresses_road_name','addresses_start_street_
                                   'neighborhood_name', 'neighborhood_code', 'gross_income'], axis=1)
 
 
-# hacemos que los centros no se repitan para cada distrito. Se duplican cuando tienen diferentes secondary_filter_name
+# Nos aseguramos que los centros no se repitan para cada distrito. (Para eso, actualizar variable secondary_filters_name para que contega 
+# todos los valores posibles para cada distrito)
 df_unificado['secondary_filters_name'] = df_unificado.groupby('name')['secondary_filters_name'].transform(lambda x: ', '.join(x))
 df_unificado = df_unificado.drop_duplicates()
 
-
+# Generamos el JSON en el formato adecuado para backend
 json_unificado = (
     df_unificado.groupby(['district_name', 'avg_income', 'district_code'])
     .apply(lambda x: x[['name', 'address', 'addresses_zip_code',
@@ -51,6 +50,5 @@ json_unificado = (
     .to_json(orient='records')
 )
 
-# Guardar el JSON en un archivo
 with open('Input_Data\df_district_information.json', 'w') as f:
     f.write(json_unificado)
