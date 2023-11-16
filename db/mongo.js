5000e018c07000e83e912fb2bfdf3916509944fc
eduardferre <eduard.ferre.sanchez@estudiantat.upc.edu>
2023-11-16 21:57:49 +0100

    no hardcoded connections

--- db/mongo.js
+++ db/mongo.js
@@ -1,12 +1,11 @@
 import mongoose from 'mongoose'
+import 'dotenv/config'
 
 export class Mongo {
   static async init() {
     console.log('Connecting to database...')
     this.connection = mongoose
-      .connect(
-        'mongodb+srv://test1:team1@cluster0.zrplbre.mongodb.net/ed_4_all?authSource=admin'
-      )
+      .connect(process.env.DB_URL)
       .then((db) => console.log('DB is connected'))
     return this.connection
   }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import { Mongo } from '../db/mongo.js'
 import cors from 'cors'
 import morgan from 'morgan'
 import { districtsRouter } from './routes/districts.js'
-import { centersRouter } from "./routes/center.js";
+import { centersRouter } from './routes/centers.js'
 // import { corsMiddleware } from './middlewares/cors.js'
 
-const PORT = process.env.PORT ?? 3001;
+const PORT = process.env.PORT
 
-const app = express();
-app.disable("x-powered-by");
+const app = express()
+app.disable('x-powered-by')
 
 // Middlewares
 app.use(json())
@@ -18,16 +18,16 @@ app.use(cors('*'))
 app.use(morgan('dev'))
 
 // Routes
-app.use("/districts", districtsRouter);
-app.use("/centers", centersRouter);
+app.use('/districts', districtsRouter)
+app.use('/centers', centersRouter)
 
 app.use((req, res) => {
-  res.status(404).send("404 - Not Found");
-});
+  res.status(404).send('404 - Not Found')
+})
 
 // Server listening
 app.listen(PORT, async () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
-  await Mongo.init();
-  console.log("Connected to database!");
-});
+  console.log(`Server listening on port http://localhost:${PORT}`)
+  await Mongo.init()
+  console.log('Connected to database!')
+})
